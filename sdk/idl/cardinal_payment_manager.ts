60a50cc835d55aa0411e90982c0bcfefd7905b3d
jpbogle <jpbogle22@gmail.com>
2022-12-20 15:34:23 -0500

    Bump version

--- sdk/idl/cardinal_payment_manager.ts
+++ sdk/idl/cardinal_payment_manager.ts
@@ -1,5 +1,5 @@
 export type CardinalPaymentManager = {
-  version: "2.1.1";
+  version: "2.2.0";
   name: "cardinal_payment_manager";
   instructions: [
     {
@@ -383,7 +383,7 @@ export type CardinalPaymentManager = {
 };
 
 export const IDL: CardinalPaymentManager = {
-  version: "2.1.1",
+  version: "2.2.0",
   name: "cardinal_payment_manager",
   instructions: [
     {
--- tools/utils.ts
+++ tools/utils.ts
@@ -1,12 +1,5 @@
 import { utils } from "@project-serum/anchor";
-import type { Wallet } from "@project-serum/anchor/dist/cjs/provider";
-import type {
-  Connection,
-  SendTransactionError,
-  Signer,
-  Transaction,
-} from "@solana/web3.js";
-import { Keypair, sendAndConfirmRawTransaction } from "@solana/web3.js";
+import { Keypair } from "@solana/web3.js";
 
 export const keypairFrom = (s: string, n?: string): Keypair => {
   try {
@@ -40,73 +33,5 @@ export const keypairFrom = (s: string, n?: string): Keypair => {
       process.stdout.write(`${n ?? "keypair"} is not valid keypair`);
       process.exit(1);
     }
-  }
-};
-
-export async function executeTransaction(
-  connection: Connection,
-  tx: Transaction,
-  wallet: Wallet,
-  signers?: Signer[],
-  silent?: boolean
-): Promise<string> {
-  tx.recentBlockhash = (await connection.getLatestBlockhash()).blockhash;
-  tx.feePayer = wallet.publicKey;
-  await wallet.signTransaction(tx);
-  if (signers) {
-    tx.partialSign(...signers);
-  }
-  try {
-    const txid = await sendAndConfirmRawTransaction(connection, tx.serialize());
-    return txid;
-  } catch (e) {
-    if (!silent) {
-      handleError(e);
-    }
-    throw e;
-  }
-}
-
-export async function executeTransactions(
-  connection: Connection,
-  txs: Transaction[],
-  wallet: Wallet,
-  signers?: Signer[]
-): Promise<string[]> {
-  const latestBlockhash = (await connection.getLatestBlockhash()).blockhash;
-  const signedTxs = await wallet.signAllTransactions(
-    txs.map((tx) => {
-      tx.recentBlockhash = latestBlockhash;
-      tx.feePayer = wallet.publicKey;
-      if (signers) {
-        tx.partialSign(...signers);
-      }
-      return tx;
-    })
-  );
-  const txids = await Promise.all(
-    signedTxs.map(async (tx) => {
-      try {
-        const txid = await sendAndConfirmRawTransaction(
-          connection,
-          tx.serialize()
-        );
-        return txid;
-      } catch (e) {
-        handleError(e);
-        throw e;
-      }
-    })
-  );
-  return txids;
-}
-
-export const handleError = (e: any) => {
-  const message = (e as SendTransactionError).message ?? "";
-  const logs = (e as SendTransactionError).logs;
-  if (logs) {
-    console.log(logs);
-  } else {
-    console.log(e, message);
   }
 };
